c4d44ee4fbae2c318e35bae26ac251b6a15b146b
Ekaterina Kuznetsova <katuxa1231@yandex.ru>
2022-09-17 23:36:55 +0400

    Добаляет логгер

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const { StatusCode } = require('./constants/api');
 const { login, createUser } = require('./controllers/users');
 const { auth } = require('./midlewares/auth');
 const { handleError } = require('./midlewares/error');
+const { requestLogger, errorLogger } = require('./midlewares/logger');
 const { validationModel } = require('./constants/validation');
 
 const { PORT = 3000 } = process.env;
@@ -20,6 +21,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(requestLogger);
 app.post('/signin', celebrate({
   body: {
     email: validationModel.user.email,
@@ -36,6 +38,7 @@ app.use('/cards', require('./routes/cards'));
 app.use('/*', (req, res) => {
   res.status(StatusCode.NOT_FOUND).send({ message: 'Путь не существует' });
 });
+app.use(errorLogger);
 app.use(errors());
 app.use(handleError);
 app.listen(PORT);
